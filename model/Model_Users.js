a8d6eafcaf94484e2e3bd20a5423a0756c2cfe71
DAFAAHMADFAHRISI <dafamangku@gmail.com>
2024-11-07 19:55:52 +0700

    menambahkan crud users

--- model/Model_Users.js
+++ model/Model_Users.js
@@ -1,23 +1,67 @@
-let db = require(`../db/config`)
+let db = require(`../db/config`);
 
-class Model_Users{
-    static getUser(email, password){
-        return new Promise((resolve, reject) => {
-            db.query(`SELECT * FROM users where username = ? && password = ? ORDER BY id_user DESC`, [email,password], (err, rows) => {
-                if(err) return reject(err)
-                resolve(rows[0])
-            })
-        })
-    }
+class Model_Users {
+  static getAll() {
+    return new Promise((resolve, reject) => {
+      db.query(`select * from users order by id_user desc`, (err, rows) => {
+        if (err) return reject(err);
+        resolve(rows);
+      });
+    });
+  }
 
-    static getUserById(id){
-        return new Promise((resolve, reject) => {
-            db.query(`SELECT * FROM users where id_user = ?`, [id], (err, rows) => {
-                if(err) return reject(err)
-                resolve(rows[0])
-            })
-        })
-    }
+  static getUser(email, password) {
+    return new Promise((resolve, reject) => {
+      db.query(
+        `SELECT * FROM users where username = ? && password = ? ORDER BY id_user DESC`,
+        [email, password],
+        (err, rows) => {
+          if (err) return reject(err);
+          resolve(rows[0]);
+        }
+      );
+    });
+  }
+
+  static getUserById(id) {
+    return new Promise((resolve, reject) => {
+      db.query(`SELECT * FROM users where id_user = ?`, [id], (err, rows) => {
+        if (err) return reject(err);
+        resolve(rows[0]);
+      });
+    });
+  }
+
+  static deleteUsers(id) {
+    return new Promise((resolve, reject) => {
+      db.query(`delete from users where id_user = ?`, [id], (err, rows) => {
+        if (err) return reject(err);
+        resolve(rows);
+      });
+    });
+  }
+
+  static storeUsers(data) {
+    return new Promise((resolve, reject) => {
+      db.query(`insert into users set ?`, data, (err, rows) => {
+        if (err) return reject(err);
+        resolve(rows);
+      });
+    });
+  }
+
+  static updateUsers(data, id) {
+    return new Promise((resolve, reject) => {
+      db.query(
+        `update users set ? where id_user = ?`,
+        [data, id],
+        (err, rows) => {
+          if (err) return reject(err);
+          resolve(rows);
+        }
+      );
+    });
+  }
 }
 
-module.exports = Model_Users
+module.exports = Model_Users;
