1f53b8b9930feafc117d395167c889945f14a135
vernandav mnrg <vernanda.mhmnrg@gmail.com>
2024-11-24 18:01:55 +0700

    membuat pilihan event dan tambahan 1 grafik

---
+++ public/javascripts/chart4.js
@@ -0,0 +1,77 @@
+const data = {
+    labels: ['Produk A', 'Produk B', 'Produk C', 'Produk D', 'Produk E', 'Produk F'],
+    datasets: [{
+        label: 'Penjualan Produk',
+        data: [85, 60, 70, 50, 95, 40],
+        backgroundColor: [
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+        'rgba(255, 159, 64, 0.2)'
+        ],
+        borderColor: [
+        'rgba(255, 99, 132, 1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)',
+        'rgba(153, 102, 255, 1)',
+        'rgba(255, 159, 64, 1)'
+        ],
+        borderWidth: 2
+    }]
+};
+
+function adjustRadiusBasedOnData(ctx) {
+    const v = ctx.parsed.y;
+    return v < 50 ? 5
+        : v < 70 ? 9
+        : v < 90 ? 12
+        : 15;
+}
+
+const config = {
+    type: 'radar',
+    data: data,
+    options: {
+        plugins: {
+        legend: {
+            labels: {
+            color: '#FFFFFF'
+            }
+        },
+        tooltip: {
+        enabled: true
+        },
+    },
+        elements: {
+        point: {
+            radius: adjustRadiusBasedOnData,
+            hoverRadius: 15,
+        }
+    },
+    scales: {
+        r: {
+            beginAtZero: true,
+            max: 100,
+            angleLines: {
+            color: 'grey'
+        },
+            grid: {
+            color: 'grey'
+        },
+        ticks: {
+            color: '#FFFFFF',
+            backdropColor: 'rgba(0, 0, 0, 0)'
+        },
+        pointLabels: {
+            color: '#FFFFFF'
+        }
+        }
+    }
+    }
+};
+
+const ctx4 = document.getElementById('radar').getContext('2d');
+new Chart(ctx4, config);
